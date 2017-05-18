892bc41cd8a04bdb37843e4e1cb7287b88da4000
renacer <gourmentt@gmail.com>
2017-05-19 00:59:54 +0400

    Changed npm commands

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,4 +46,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+	grunt.registerTask('buildProdAssets', ['jst', 'concat', 'uglify', 'cssmin']);
 };
